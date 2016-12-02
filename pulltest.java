c31292b4dcc6c36c726cb29bd2cd725080ae1483
darrkyo <darrkyo@sina.com>
2016-12-02 14:37:15 +0800

    first

--- pulltest.java
+++ pulltest.java
@@ -1,5 +1,6 @@
 public class pulltest{
   public static void main(String[] args) {
-        System.out.println("Hellow pulltest!");
+      System.out.println("Hellow pulltest!");
+      System.out.println("Hello!");
     }
 }
